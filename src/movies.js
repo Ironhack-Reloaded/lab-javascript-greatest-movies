107a6fecf26deb943b953080198a5f00a3968565
SilviuDN <silviudilimot@gmail.com>
2022-01-17 08:24:55 +0100

    task 5 done

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,23 @@
+const movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  }
+]
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -19,13 +39,53 @@ function howManyMovies(movies, director = 'Steven Spielberg') {
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if( movies.length === 0 ){
+    return 0
+  }
+  const suma = movies.reduce( ( sum, movie ) => sum + movie.score , 0)
+  const average = Number.parseFloat( suma ) / movies.length 
+  console.log("object")
+  return Number.parseFloat( average.toFixed(2) )
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies, genre = 'Drama') {
+  if( movies.length === 0 ){
+    return 0
+  }
+  let dramaMovies = movies.filter( movie => movie.genre.includes(genre))
+  let average =  scoresAverage( dramaMovies )
+  return Number.parseFloat( average.toFixed(2) )
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  if( movies.length === 0 ){
+    return []
+  }
+
+  const ordered = [...movies.sort( (a,b) => {
+    if(a.year - b.year){ return (a.year - b.year) }
+    return a.title.localeCompare(b.title)
+  } )]
+
+  return ordered  
+}
+
+// function orderByYear(moviesArr) { 
+//   if( movies.length === 0 ){
+//     return null
+//   }
+//   const moviesChronologicalyOrdered = [... moviesArr.sort( (movie1, movie2) => {
+//     if( movie1.year - movie2.year){
+//       return movie1.year - movie2.year
+//     }
+//     return movie1.title.localeCompare( movie2.title)
+//   }) ]
+//   return moviesChronologicalyOrdered
+// }
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
